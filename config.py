beeb827bc59d48c035f28a0afe6c64ad59bfebac
konarovic <89127737+Konarovic@users.noreply.github.com>
2024-04-09 15:04:06 +0200

    reorganisation

---
+++ config.py
@@ -0,0 +1,15 @@
+# config.py
+
+import os
+
+# Chemin du répertoire racine du projet
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Chemins
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+DATA_CSV = os.path.join(DATA_DIR, 'maintenance_data.csv')
+
+
+
+
+
