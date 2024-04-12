7fd26b54b0b363de7733e7a287c9961987f73b44
konarovic <89127737+Konarovic@users.noreply.github.com>
2024-04-12 14:30:39 +0200

    Modelisation + conf mat

--- src/utils/Functions.py
+++ src/utils/Functions.py
@@ -1,13 +1,14 @@
-from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, roc_auc_score
 
 
 def get_metrics(y_true, y_pred):
-    f1_score_class = f1_score(y_true, y_pred, average='None')
+    f1_score_class = f1_score(y_true, y_pred, average=None)
     dic_metrics = {
         'Accuracy': accuracy_score(y_true, y_pred),
         'Recall': recall_score(y_true, y_pred, average='macro'),
         'Precision': precision_score(y_true, y_pred, average='macro'),
         'F1': f1_score(y_true, y_pred, average='macro'),
         'F1 Scores per Class': f1_score_class
+        
     }
     return dic_metrics
