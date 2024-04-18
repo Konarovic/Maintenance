29a0e5930f37e877435f9b29d145202927c8f3a4
konarovic <89127737+Konarovic@users.noreply.github.com>
2024-04-18 11:49:17 +0200

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -16,36 +16,3 @@ st.write("\n\n")
 # Load the model
 with open('../model/random_forest_model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
-
-# Streamlit interface to input data
-col1, col2 = st.columns(2)
-
-with col1:
-    air = st.number_input(label='Air Temperature')
-    process = st.number_input(label='Process Temperature')
-    rpm = st.number_input(label='Rotational Speed')
-
-with col2:
-    torque = st.number_input(label='Torque')
-    tool_wear = st.number_input(label='Tool Wear')
-    type = st.selectbox(label='Type', options=['Low', 'Medium', 'High'])
-
-# Function to predict the input
-def prediction(air, process, rpm, torque, tool_wear, type):
-    # Create a df with input data
-    df_input = pd.DataFrame({
-        'Air_temperature': [air],
-        'Process_temperature': [process],
-        'Rotational_speed': [rpm],
-        'Torque': [torque],
-        'Tool_wear': [tool_wear],
-        'Type': [type]
-    })
-    
-    prediction = model.predict(df_input)
-    return prediction
-
-# Botton to predict
-if st.button('Predict'):
-    predict = prediction(air, process, rpm, torque, tool_wear, type)
-    st.success(predict)
