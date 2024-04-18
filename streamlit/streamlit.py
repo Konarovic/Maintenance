c7d3a454d418384ed86e1f73b523750727a3c0b6
konarovic <89127737+Konarovic@users.noreply.github.com>
2024-04-18 14:32:55 +0200

    model update
    lgbmc
    streamlit

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -3,8 +3,8 @@ import streamlit as st
 import pandas as pd
 
 
-# Page title
-st.title('Maintenance - Failure Prediction')
+# Page 
+st.title('Maintenance')
 col1, col2, col3 = st.columns([1,4,1])
 with col1:
     st.write("")
@@ -15,3 +15,7 @@ with col3:
 st.write("\n\n")
 
 
+# chargement du modele
+
+with open('../model/lgbmc.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
