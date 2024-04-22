4e336bc10d0f52997df41199bf674ad9599c1cbe
konarovic <89127737+Konarovic@users.noreply.github.com>
2024-04-22 17:09:53 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -75,6 +75,8 @@ if option == '1 / Prédictions':
 # Page 2
 elif option == '2 / Détails du modèle':
     st.write('wip')
-    st.dataframe('./data/maintenance_data_clean.csv')
+    
+    df = pd.read_csv('./data/maintenance_data_clean.csv')
+    st.dataframe(df)
   
 
