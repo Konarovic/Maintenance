d582cdf9a14be5d98ddddca14e9b0211ccb1fc34
konarovic <89127737+Konarovic@users.noreply.github.com>
2024-04-22 16:36:25 +0200

    streamlit tab

--- streamlit.py
+++ streamlit.py
@@ -2,9 +2,16 @@ import joblib
 import streamlit as st
 import pandas as pd
 
+# Charger le modèle
+try:
+    model = joblib.load('./model/lgbmc_model.pkl')
+except FileNotFoundError:
+    st.error("Le fichier du modèle est introuvable.")
+except Exception as e:
+    st.error(f"Une erreur s'est produite lors du chargement du modèle : {e}")
+
 
 # Page 
-
 col1, col2, col3 = st.columns([1,4,1])
 with col1:
     st.write("")
@@ -15,12 +22,31 @@ with col3:
     st.write("")
 st.write("\n\n")
 
-
+# menu
 option = st.sidebar.selectbox(
     'Navigation',
     ('1 / Prédictions', '2 / Details du modèle')
 )
 
 
-#impoprt du modele
-lgbmc_model = joblib.load('./model/lgbmc_model.pkl')
+# Page 1
+if option == '1 / Prédictions':
+    st.write('Tableau de prédictions')
+   
+    col1,col2 = st.columns(2)
+
+    with col1 :
+        air = st.number_input(label='Air Temperature')
+        process = st.number_input(label='Process Temperature')
+        rpm = st.number_input(label='Rotational Speed')
+
+    with col2:
+        torque = st.number_input(label='Torque')
+        tool_wear = st.number_input(label='Tool Wear')
+        type = st.selectbox(label='Type', options=['Low', 'Medium', 'High'])
+
+# Page 2
+elif option == '2 / Détails du modèle':
+    st.write('wip')
+  
+
