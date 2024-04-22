0b4d4958b92d7521a5f4e3186bc1e55986155c39
konarovic <89127737+Konarovic@users.noreply.github.com>
2024-04-22 17:04:05 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -16,7 +16,7 @@ col1, col2, col3 = st.columns([1,4,1])
 with col1:
     st.write("")
 with col2 :
-    st.title('Maintenance')
+    st.title('Maintenance (WIP)')
     st.subheader('Prédiction de pannes')
 with col3:
     st.write("")
